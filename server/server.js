ce4823a46ff7802792fcd2b5c7a26a8ff9186bd9
Sean Ippolito <ippolitosean@gmail.com>
2023-05-03 02:55:09 -0700

    - add react client and link it with apollo and graphql

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./server/utils/auth');
+const { authMiddleware } = require('./utils/auth');
+const path = require('path');
+
 
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./server/schemas');
-const db = require('./server/config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -24,6 +26,15 @@ const startApolloServer = async (typeDefs, resolvers, context) => {
   await server.start();
 // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({app});
+
+  // Serve up static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
   db.once('open', () => {
     app.listen(PORT, () => {
